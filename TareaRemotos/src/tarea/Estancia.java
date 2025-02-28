c9b7ceacd29d3ec433117dcc32602f41567917ba
DiegoGarciaAbril <dgarciabril15@gmail.com>
2025-02-28 11:39:33 +0100

    Clase estancia creada

--- TareaRemotos/src/tarea/Estancia.java
+++ TareaRemotos/src/tarea/Estancia.java
@@ -2,4 +2,20 @@ package tarea;
 
 public class Estancia {
 
+	String nombre;
+	double metrosCuadrados;
+	int numPuertas;
+	int numVentanas;
+	
+	public Estancia (String n, double m2, int numP, int numV) {
+		this.nombre = n;
+		this.metrosCuadrados = m2;
+		this.numPuertas = numP;
+		this.numVentanas = numV;
+		
+	}
+	
+	public double getMetrosCuadrados() {
+		return metrosCuadrados;
+	}
 }
